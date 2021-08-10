ba128b765d005e062437c875e3c52e9f2defd2e2
kutsulab <kutsu920@gmail.com>
2021-08-10 20:40:09 +0900

    구구단을 출력하는 [Gugudan.java]파일 생성. 해당 파일은 원시적인 구구단 출력파일로, 2~9단을 일일이 입력하여 그대로 출력하도록 함.

---
+++ src/Gugudan.java
@@ -0,0 +1,93 @@
+
+public class Gugudan {
+	public static void main(String[] args) {
+		//2단
+			System.out.println("//////2단");
+			System.out.println("2 * 1 = "+2*1);
+			System.out.println("2 * 2 = "+2*2);
+			System.out.println("2 * 3 = "+2*3);
+			System.out.println("2 * 4 = "+2*4);
+			System.out.println("2 * 5 = "+2*5);
+			System.out.println("2 * 6 = "+2*6);
+			System.out.println("2 * 7 = "+2*7);
+			System.out.println("2 * 8 = "+2*8); 
+			System.out.println("2 * 9 = "+2*9);	
+		//3단
+			System.out.println("//////3단//////");
+			System.out.println("3 * 1 = "+3*1);
+			System.out.println("3 * 2 = "+3*2);
+			System.out.println("3 * 3 = "+3*3);
+			System.out.println("3 * 4 = "+3*4);
+			System.out.println("3 * 5 = "+3*5);
+			System.out.println("3 * 6 = "+3*6);
+			System.out.println("3 * 7 = "+3*7);
+			System.out.println("3 * 8 = "+3*8); 
+			System.out.println("3 * 9 = "+3*9);	
+		//4단
+			System.out.println("//////4단//////");
+			System.out.println("4 * 1 = "+4*1);
+			System.out.println("4 * 2 = "+4*2);
+			System.out.println("4 * 3 = "+4*3);
+			System.out.println("4 * 4 = "+4*4);
+			System.out.println("4 * 5 = "+4*5);
+			System.out.println("4 * 6 = "+4*6);
+			System.out.println("4 * 7 = "+4*7);
+			System.out.println("4 * 8 = "+4*8); 
+			System.out.println("4 * 9 = "+4*9);	
+		//5단
+			System.out.println("//////5단//////");
+			System.out.println("5 * 1 = "+5*1);
+			System.out.println("5 * 2 = "+5*2);
+			System.out.println("5 * 3 = "+5*3);
+			System.out.println("5 * 4 = "+5*4);
+			System.out.println("5 * 5 = "+5*5);
+			System.out.println("5 * 6 = "+5*6);
+			System.out.println("5 * 7 = "+5*7);
+			System.out.println("5 * 8 = "+5*8); 
+			System.out.println("5 * 9 = "+5*9);	
+		//6단
+			System.out.println("//////6단//////");
+			System.out.println("6 * 1 = "+6*1);
+			System.out.println("6 * 2 = "+6*2);
+			System.out.println("6 * 3 = "+6*3);
+			System.out.println("6 * 4 = "+6*4);
+			System.out.println("6 * 5 = "+6*5);
+			System.out.println("6 * 6 = "+6*6);
+			System.out.println("6 * 7 = "+6*7);
+			System.out.println("6 * 8 = "+6*8); 
+			System.out.println("6 * 9 = "+6*9);	
+		//7단
+			System.out.println("//////7단//////");
+			System.out.println("7 * 1 = "+7*1);
+			System.out.println("7 * 2 = "+7*2);
+			System.out.println("7 * 3 = "+7*3);
+			System.out.println("7 * 4 = "+7*4);
+			System.out.println("7 * 5 = "+7*5);
+			System.out.println("7 * 6 = "+7*6);
+			System.out.println("7 * 7 = "+7*7);
+			System.out.println("7 * 8 = "+7*8); 
+			System.out.println("7 * 9 = "+7*9);	
+		//8단
+			System.out.println("//////8단//////");
+			System.out.println("8 * 1 = "+8*1);
+			System.out.println("8 * 2 = "+8*2);
+			System.out.println("8 * 3 = "+8*3);
+			System.out.println("8 * 4 = "+8*4);
+			System.out.println("8 * 5 = "+8*5);
+			System.out.println("8 * 6 = "+8*6);
+			System.out.println("8 * 7 = "+8*7);
+			System.out.println("8 * 8 = "+8*8); 
+			System.out.println("8 * 9 = "+8*9);	
+		//9단
+			System.out.println("//////9단//////");
+			System.out.println("9 * 1 = "+9*1);
+			System.out.println("9 * 2 = "+9*2);
+			System.out.println("9 * 3 = "+9*3);
+			System.out.println("9 * 4 = "+9*4);
+			System.out.println("9 * 5 = "+9*5);
+			System.out.println("9 * 6 = "+9*6);
+			System.out.println("9 * 7 = "+9*7);
+			System.out.println("9 * 8 = "+9*8); 
+			System.out.println("9 * 9 = "+9*9);	
+	}
+}
