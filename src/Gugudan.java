dc358a32dfccdbbbf7be15e9b5d9d891cbdd5052
kutsulab <kutsu920@gmail.com>
2021-08-13 18:53:17 +0900

    [Gugudan.java] : 몇 단 부터 몇 단까지 출력할지 물어보게 하여 해당 단을 출력하도록 변경

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
+		
 			Scanner sc = new Scanner(System.in);
-			System.out.println("몇 단을 입력하시겠습니까?");
-			int dan = sc.nextInt();
+			System.out.println("몇 단부터?");
+			int firstdan = sc.nextInt();
+			System.out.println("몇 단까지?");
+			int finaldan = sc.nextInt();
 			
 			sc.close();
 			
-			System.out.println("======"+dan+"단 출력 시작======");
-			int n = 1;
-			while (n<10) {
-				System.out.println(dan+" * "+n+" = "+dan*n);
-				n ++;
+			System.out.println("++++++ 출력 시작 ++++++");
+			System.out.println();
+			for (int k=firstdan ; k<= finaldan ; k++) {
+				System.out.println("======"+k+"단======");
+				for (int j=1; j < 10; j++) {
+					System.out.println(k+" * "+j+" = "+(k*j));
+				}
 			}
-			System.out.println("======"+dan+"단 출력 끝======");
+			System.out.println();
+			System.out.println("++++++ 출력 끝 ++++++");
 	}
 }
