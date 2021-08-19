6323e7cf3d5c9b9997b61124d2de20a7dd08e558
kutsulab <kutsu920@gmail.com>
2021-08-19 20:30:09 +0900

    calendar 패키지 변경 1. 해당 달의 시작요일을 입력하면 그 요일에 맞게 달력이 출력되도록 변경함. 2. 구현방법 : 요일을 문자(str_weekday)로 받고, 이를 공백 출력 횟수(weekday)로 환산함. 공백 출력 횟수만큼 달력  시작부에서 공백을 입력함. 달력의 줄바꿈 위치의 경우, 날짜를 7로 나눈 나머지가, (7-weekday)를 7로 나눈 나머지와 같을 때  줄바꿈이 생기도록 하여 정상적으로 줄바꿈이 생기도록 함.

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -21,7 +21,7 @@ public class Calendar {
 		}
 	}
 
-	public static int getMaxDaysOfMonth(int year, int month) {
+	private int getMaxDaysOfMonth(int year, int month) {
 
 		if (isLeapYear(year)) {
 			return LEAP_MAX_DAYS[month - 1];
@@ -30,24 +30,35 @@ public class Calendar {
 		}
 	}
 
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("=======<<%4d년%3d월>>========\n", year, month);
 		System.out.printf("%3s %3s %3s %3s %3s %3s %3s\n", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
 		System.out.println("----------------------------");
 
 		int maxDays = getMaxDaysOfMonth(year, month);
 
+		// 공백 입력부
+		if (weekday >= 1) {
+			for (int i = 1; i <= weekday; i++) {
+				System.out.printf("%3s ", "");
+			}
+		}
+		// 출력부
 		for (int i = 1; i <= maxDays; i++) {
 			System.out.printf("%3d ", i);
 
-			// 7로 나눈 나머지가 0인 경우(7,14,21,28일) 줄바꿈
-			if (i % 7 == 0) {
+			
+			// 7-weekday와 비교하면 안 되는 이유
+			// 시작하는 날짜가 일요일이 되버리면
+			// weekday=0인데, 이 경우 줄바꾸는 날짜는 7, 14, 21, 28이지만
+			// 줄바꿈 날짜%7 = 0이고, 7-weekday는 7이 되버림.
+			// 따라서 7-weekday를 다시 또 7로 나눴을 때의 값과 비교해야 일반화 가능함
+			
+		    //날짜를 7로 나눈 나머지가 (7-weekday)를 7로 나눈 나머지와 같을 때
+			if ((i % 7) == (7 - weekday) % 7) {
 				System.out.println();
-
 			}
-
 		}
 		System.out.println();
 	}
-
 }
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -2,70 +2,108 @@ package calendar;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @kutsulab 
+ * str_weekday : 입력한 요일
+ * parse_weekday : week를 숫자로 환산한 값 (0 : 일요일 / 6 : 토요일)
+ **/
+
 public class Prompt {
-	
-	//private final static String PROMPT = "cal>";
-	
+	public int parse_weekday(String str_weekday) {
+		switch (str_weekday) {
+		default : return -2;
+		case "일": return 0;
+		case "월": return 1;
+		case "화": return 2;
+		case "수": return 3;
+		case "목": return 4;
+		case "금": return 5;
+		case "토": return 6;
+		case "그만해" : return -1;
+		}
+	}
+
 	public void runPrompt() {
 		Calendar cal = new Calendar();
 		Scanner scanner = new Scanner(System.in);
 
-		int year, month;
-		
-		
+		int year, month, weekday;
+		String str_weekday;
+
 		// 입력부 : 한계점 (int 타입의 변수가 아닐 경우 예외처리하지 못 함)
-		
+
 		do {
 			// 년도 입력부
 			do {
 				System.out.println("년도를 입력해주세요. (-1 입력 시 종료)");
-				System.out.print("YEAR>");
+				System.out.print(("YEAR>"));
 				year = scanner.nextInt();
-				if (year <-1) {
+				scanner.nextLine();
+
+				if (year < -1) {
 					continue;
-				}
-				else {
+				} else {
 					break;
 				}
 			} while (true);
-			
-			if (year == -1) break;
-			
-			
+
+			if (year == -1) {
+				break;
+			}
+
 			// 월 입력부
 			do {
-				System.out.println("월을 입력하면 달력을 출력합니다. 월을 입력해주세요. (-1 입력 시 종료)");
-				System.out.print("MONTH>");
+				System.out.println("월을 입력해주세요. (-1 입력 시 종료)");
+				System.out.print(("MONTH>"));
 				month = scanner.nextInt();
-				
-				if (month ==0 || month <-1||month >12) {
+				scanner.nextLine();
+
+				if (month == 0 || month < -1 || month > 12) {
 					continue;
 				}
-				
+
 				else {
 					break;
 				}
+
+			} while (true);
+
+			if (month == -1) {
+				break;
+			}
+
+			do {
+				System.out.println("첫번째 날의 요일을 입력하세요. (일, 월, 화, 수, 목, 금, 토) / (그만해 입력 시 종료)");
+				System.out.print(("WEEKDAY>"));
+				str_weekday = scanner.nextLine();
+				
+				weekday = parse_weekday(str_weekday);
+				
+				if (weekday>=-1) break;
+				else continue;	
 				
 			} while (true);
+
 			
-			if (month == -1) break;
-			else {
-				cal.printCalendar(year, month);
+			// 그만해 입력하면 do문 탈출 하게
+			if (weekday==-1) {
+				break;
 			}
-	
-		
+			
+			else {
+				cal.printCalendar(year, month, weekday);
+			}
+			
 		} while (true);
-		
 
 		scanner.close();
 		System.out.println("끝!");
-
 	}
-	
-	
+
 	public static void main(String[] args) {
-		
-		//셸 입력
+
+		// 셸 입력
 		Prompt p = new Prompt();
 		p.runPrompt();
 	}
