1cda195b19d68e3099e086c4b3498d45febadfac
kutsulab <kutsu920@gmail.com>
2021-08-19 09:45:03 +0900

    Calendar 패키지 변경 1. 년도와 월을 입력했을 시 달력을 출력하도록 변경(일요일이 1일인 달력) 2. 윤년일 때 2월을 29일까지 출력하도록 변경 3. [Prompt.java] : 년도입력부, 월 입력부에서 -1을 입력 시 종료 4.[Calendar.java] : 년도 input값에 따라 윤년일 경우 윤년의 월별 최대일수 배열을 불러오고, 그 배열에서 해당 월의 최대일 수를 불러와 달력을 출력하도록 변경

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,22 +1,44 @@
 package calendar;
 
 public class Calendar {
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	private static final int[] maxDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static boolean isLeapYear(int year) {
 
-	public static int getMaxDaysOfMonth(int month) {
-		return maxDays[month - 1];
+		// 서력 기준 년도가
+		// 4의 배수 中
+		// 1. 4의 배수 이고 400의 배수이면 윤년
+		// 2. 4의 배수 이고 100의 배수이고 400의 배수가 아니면 평년
+		// 3. 4의 배수 중 100의 배수가 아니면 윤년
+
+		// 윤년의 조건 : 4의 배수 중 100의 배수 중 400의 배수이거나, 4의 배수 중 100의 배수가 아닌 것
+
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public static void printCalendar(int year, int month) {
-		System.out.printf("===<<%4d년%3d월>>====\n", year, month);
-		System.out.printf("%s");
-		System.out.println("--------------------");
+	public static int getMaxDaysOfMonth(int year, int month) {
 
-		int maxDays = getMaxDaysOfMonth(month);
+		if (isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
+	}
+
+	public void printCalendar(int year, int month) {
+		System.out.printf("=======<<%4d년%3d월>>========\n", year, month);
+		System.out.printf("%3s %3s %3s %3s %3s %3s %3s\n", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+		System.out.println("----------------------------");
+
+		int maxDays = getMaxDaysOfMonth(year, month);
 
 		for (int i = 1; i <= maxDays; i++) {
-			System.out.printf("%3d", i);
+			System.out.printf("%3d ", i);
 
 			// 7로 나눈 나머지가 0인 경우(7,14,21,28일) 줄바꿈
 			if (i % 7 == 0) {
--- src/calendar/Prompt.java
+++ src/calendar/Prompt.java
@@ -4,32 +4,59 @@ import java.util.Scanner;
 
 public class Prompt {
 	
-	private final static String PROMPT = "cal>";
+	//private final static String PROMPT = "cal>";
 	
 	public void runPrompt() {
 		Calendar cal = new Calendar();
 		Scanner scanner = new Scanner(System.in);
 
-		int month;
-
-		// 달을 입력하면 그 달의 달력을 출력 (1일은 일요일로 가정)
-		while (true) {
-			System.out.println("달을 입력하면 그 달의 달력을 출력합니다. (1일이 일요일)");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-
-			if (month > 12 || month == 0 || month <= -2) {
-				continue;
-			}
-
-			else if (month == -1) {
-				break;
-			}
-
+		int year, month;
+		
+		
+		// 입력부 : 한계점 (int 타입의 변수가 아닐 경우 예외처리하지 못 함)
+		
+		do {
+			// 년도 입력부
+			do {
+				System.out.println("년도를 입력해주세요. (-1 입력 시 종료)");
+				System.out.print("YEAR>");
+				year = scanner.nextInt();
+				if (year <-1) {
+					continue;
+				}
+				else {
+					break;
+				}
+			} while (true);
+			
+			if (year == -1) break;
+			
+			
+			// 월 입력부
+			do {
+				System.out.println("월을 입력하면 달력을 출력합니다. 월을 입력해주세요. (-1 입력 시 종료)");
+				System.out.print("MONTH>");
+				month = scanner.nextInt();
+				
+				if (month ==0 || month <-1||month >12) {
+					continue;
+				}
+				
+				else {
+					break;
+				}
+				
+			} while (true);
+			
+			if (month == -1) break;
 			else {
-				Calendar.printCalendar(2021,month);
+				cal.printCalendar(year, month);
 			}
-		}
+	
+		
+		} while (true);
+		
+
 		scanner.close();
 		System.out.println("끝!");
 
