62fb08d6025b6b184758abe939b3d06c9387993a
kutsulab <kutsu920@gmail.com>
2021-08-20 15:04:06 +0900

    calendar 패키지 수정 // 수정내역 : 1. 년, 월 입력하면 해당 달의 달력이 출력되도록 수정 2. 매커니즘 : 서기 1년 1월 1일 월요일 기준 경과일을 통해 계산

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,6 +1,7 @@
 package calendar;
 
 public class Calendar {
+
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
@@ -30,32 +31,77 @@ public class Calendar {
 		}
 	}
 
-	public void printCalendar(int year, int month, int weekday) {
+	private int getYearFirstweekday(int year) {
+
+		int leapyearcount;
+		int multiple4, multiple100, multiple400;
+
+		multiple4 = (int) ((year - 1) / (4.0));
+		multiple100 = (int) ((year - 1) / (100.0));
+		multiple400 = (int) ((year - 1) / (400.0));
+
+		// 서기 1년부터 전년까지의 윤년의 갯수
+		leapyearcount = multiple4 - (multiple100 - multiple400);
+
+		// 서기 1년 1월 1일로부터 해당 년 1월 1일까지 경과한 일 수
+		int daycount = (365 * (year - 1 - leapyearcount)) + (366 * (leapyearcount));
+
+		// daycount_rest : daycount를 7로 나눈 나머지
+		int daycount_rest = daycount % 7;
+
+		return (1 + daycount_rest) % 7;
+	}
+
+	int getMonthFirstweekday(int year, int month) {
+
+		int YearFirstweekday = getYearFirstweekday(year);
+
+		// 해당년 1월 1일로부터 그 달 1일까지 경과한 일 수
+		int daycount = 0;
+
+		if (month >= 2) {
+			for (int i = 1; i < month; i++) {
+				daycount += getMaxDaysOfMonth(year, i);
+			}
+			int daycount_rest = daycount % 7;
+
+			return (YearFirstweekday + daycount_rest) % 7;
+		}
+
+		else {
+			return YearFirstweekday;
+		}
+	}
+
+// 달력 출력부
+	void printCalendar(int year, int month) {
 		System.out.printf("=======<<%4d년%3d월>>========\n", year, month);
 		System.out.printf("%3s %3s %3s %3s %3s %3s %3s\n", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
 		System.out.println("----------------------------");
 
 		int maxDays = getMaxDaysOfMonth(year, month);
-
+		int monthFirstweekday = getMonthFirstweekday (year, month);
+		
+		
 		// 공백 입력부
-		if (weekday >= 1) {
-			for (int i = 1; i <= weekday; i++) {
+		if (monthFirstweekday >= 1) {
+			for (int i = 1; i <= monthFirstweekday; i++) {
 				System.out.printf("%3s ", "");
 			}
 		}
+
 		// 출력부
 		for (int i = 1; i <= maxDays; i++) {
 			System.out.printf("%3d ", i);
 
-			
-			// 7-weekday와 비교하면 안 되는 이유
+			// 7-monthFirstweekday와 비교하면 안 되는 이유
 			// 시작하는 날짜가 일요일이 되버리면
-			// weekday=0인데, 이 경우 줄바꾸는 날짜는 7, 14, 21, 28이지만
-			// 줄바꿈 날짜%7 = 0이고, 7-weekday는 7이 되버림.
-			// 따라서 7-weekday를 다시 또 7로 나눴을 때의 값과 비교해야 일반화 가능함
-			
-		    //날짜를 7로 나눈 나머지가 (7-weekday)를 7로 나눈 나머지와 같을 때
-			if ((i % 7) == (7 - weekday) % 7) {
+			// monthFirstweekday=0인데, 이 경우 줄 바꾸는 날짜는 7, 14, 21, 28이지만
+			// 줄바꿈 날짜%7 = 0이고, 7-monthFirstweekday는 7이 되버림.
+			// 따라서 7-monthFirstweekday를 다시 또 7로 나눴을 때의 값과 비교해야 일반화 가능함
+
+			// 날짜를 7로 나눈 나머지가 (7-monthFirstweekday)를 7로 나눈 나머지와 같을 때
+			if ((i % 7) == (7 - monthFirstweekday) % 7) {
 				System.out.println();
 			}
 		}
