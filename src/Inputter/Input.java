804704038bf9dc7ee158b6cba52a661141a807e5
kutsulab <kutsu920@gmail.com>
2021-09-01 19:35:47 +0900

    값을 입력하게 하고, 입력한 값의 유효성을 검사한뒤 반환하는 Inputter 작성 시작

---
+++ src/Inputter/Input.java
@@ -0,0 +1,19 @@
+package Inputter;
+import java.util.Scanner;
+
+class Input {
+	Scanner scanner = new Scanner(System.in);
+	
+	public static int getInt(boolean condition) {
+		
+		//1. 값 입력 요청//
+		//2. 값 입력//
+		//3. 값 입력
+		//4. 값의 유효성 검사//
+		//5. 정상적인 값을 입력했을 경우 반환//
+		
+		return 0;
+	}
+	
+}
+
