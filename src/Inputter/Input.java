75183106b5369473b306e76ffe729fdf2cf39aa0
kutsulab <kutsu920@gmail.com>
2021-09-02 10:29:36 +0900

    Inputter : Input.class의 getInt() 메서드가 Request_Int 인터페이스를 참조하도록 함. 정수가 아닌 값을 입력했을 때의 예외 메시지, 유효 정수값 범위, 유효하지 않은 정수값을 입력했을 때의 메시지 등은 Request_Int를 구현한 메서드에서 설정하도록 하면 됨.

--- src/Inputter/Input.java
+++ src/Inputter/Input.java
@@ -4,44 +4,46 @@ import java.util.Scanner;
 
 class Input {
 	static Scanner scanner = new Scanner(System.in);
-	
+
 	public static int getInt() {
-		//1. 값 입력 요청 + 값 입력 + int 타입이 아닌 값을 입력했을 때에 대한 예외처리
-		int temp_int = getTemp_int();
+		
+		int tempInt = 0;
+		int returnInt = 0;
+		Request_Int rqi = new GuGuDanRequest();
 
-		// 2. 값의 유효성 검사 (추가 안 함)
+		do {
+			// 1. 값 입력 요청 + 값 입력 + int 타입이 아닌 값을 입력했을 때에 대한 예외처리
+			tempInt = getTemp_int(rqi);
 
+			// 2. 값의 유효성 검사
+			if (rqi.int_condition(tempInt)) {
+				returnInt = tempInt;
+				break;
+			} else {
+				rqi.notValidIntMessage();
+				continue;
+			}
+		} while (true);
+		
 		// 3. 유효한 값을 입력했을 경우 반환
-		int returnInt = temp_int;
 		return returnInt;
 	}
-	
-	static int getTemp_int() {	
-		int temp_int;
+
+	private static int getTemp_int(Request_Int rqi) {
+		int tempInt = 0;
 		
 		do {
 			try {
-				Request r = new GuGuDanRequest();
-				r.request();
-				temp_int = scanner.nextInt();
+				rqi.request();
+				tempInt = scanner.nextInt();
 				scanner.nextLine();
 			} catch (Exception e) {
 				scanner.nextLine();
-				int_exceptionMessage();
+				rqi.exceptionMessage(); // int 타입이 아닌 값을 입력했을 때의 경고 메세지
 				continue;
-			}		
-			
+			}
 			break;
-			
 		} while (true);
-
-		return temp_int;
+		return tempInt;
 	}
-
-
-	static void int_exceptionMessage() {
-		System.out.println("정수가 아닌 값을 입력했습니다. 정수를 입력하세요.");
-		return;
-	}
-	
 }
--- src/Inputter/Request.java
+++ src/Inputter/Request.java
@@ -1,16 +1,42 @@
 package Inputter;
 
 interface Request {
-	public abstract void request();
+	void request();
+}
+
+interface Request_Int extends Request {
+	void exceptionMessage();
+	boolean int_condition(int i);
+	void notValidIntMessage();
 }
 
 
 //Sample
-class GuGuDanRequest implements Request {
+class GuGuDanRequest implements Request_Int {
+
 	
 	@Override
 	public void request() {
-		System.out.println("2이상 9이하의 정수를 입력해주세요.");
+		System.out.println("=================================");
+		System.out.println("2 이상 9 이하의 정수를 입력해주세요.");
+		System.out.print("> ");
+	}
+	
+	@Override
+	public void exceptionMessage() {
+		System.out.println("정수가 아닌 값을 입력했습니다. 정수를 다시 입력하세요.");
+		return;
+	}
+	
+	@Override
+	public boolean int_condition(int i) {
+		return i<=9&&i>=2;
+	}
+	
+	@Override
+	public void notValidIntMessage() {
+		System.out.println("잘못 입력했습니다. 2 이상 9 이하의 정수를 다시 입력해주세요.");
 	}
 	
 }
+
